641bc829b133499b9807f40bf46c0775349649af
sfggutierrez-glitch <sfg.gutierrez@yavirac.edu.ec>
2026-08-10 12:29:55 -0500

    feat: crear pantalla de login con correo institucional y navegacion a dashboard

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Login } from './features/auth/login/login';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [Login],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+
+}
